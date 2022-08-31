5bb23ff8f6eda05a3cc83751c24232b7269196f1
hklrzy <hk__lrzy@163.com>
2022-08-31 23:49:12 +0800

    [SeaTunnel][Connector-V2]Support doris writer by seatunnel sink.

--- seatunnel-connectors-v2/connector-doris/src/main/java/org/apache/seatunnel/connectors/doris/sink/DorisWriter.java
+++ seatunnel-connectors-v2/connector-doris/src/main/java/org/apache/seatunnel/connectors/doris/sink/DorisWriter.java
@@ -1,9 +1,27 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.seatunnel.connectors.doris.sink;
 
 import org.apache.seatunnel.api.sink.SinkWriter;
 import org.apache.seatunnel.api.table.type.SeaTunnelRow;
 import org.apache.seatunnel.api.table.type.SeaTunnelRowType;
 import org.apache.seatunnel.common.utils.JsonUtils;
+import org.apache.seatunnel.connectors.doris.common.DorisConstants;
 import org.apache.seatunnel.connectors.doris.common.DorisOptions;
 import org.apache.seatunnel.connectors.doris.sink.loader.DorisStreamLoader;
 import org.apache.seatunnel.connectors.seatunnel.common.sink.AbstractSinkWriter;
@@ -16,8 +34,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 /**
  * Created 2022/8/01
@@ -25,12 +47,16 @@ import java.util.List;
 public class DorisWriter extends AbstractSinkWriter<SeaTunnelRow, Void> {
     private static final Logger LOG = LoggerFactory.getLogger(DorisWriter.class);
 
+    private static final String LABEL_TEMPLATE = "setunnel_sink_subtask_%s_%s_%s";
+
     private final SeaTunnelRowType seaTunnelRowType;
     private final DorisOptions options;
     private final JsonSerializationSchema serializationSchema;
     private final DorisLoader<String> loader;
 
     private final List<JsonNode> batch;
+    private final DateTimeFormatter formatter;
+    private final SinkWriter.Context context;
 
     public DorisWriter(Config dorisSinkConf,
                        SeaTunnelRowType seaTunnelRowType,
@@ -41,7 +67,10 @@ public class DorisWriter extends AbstractSinkWriter<SeaTunnelRow, Void> {
         //now we only support stream load, maybe future broker load will implement in seatunnel.
         this.loader = new DorisStreamLoader(options);
         this.batch = new ArrayList<>();
-        LOG.info("Subtask {} create writer.", context.getIndexOfSubtask());
+        this.formatter = DateTimeFormatter.ofPattern(DorisConstants.DORIS_LABEL_PATTERN_VALUE)
+            .withZone(ZoneId.systemDefault());
+        this.context = context;
+        LOG.info("Subtask {} create doris writer.", context.getIndexOfSubtask());
     }
 
     @Override
@@ -54,7 +83,9 @@ public class DorisWriter extends AbstractSinkWriter<SeaTunnelRow, Void> {
 
     private synchronized void flush() {
         String request = JsonUtils.toJsonString(batch);
-        loader.load(request);
+        String label = String.format(LABEL_TEMPLATE, context.getIndexOfSubtask(),
+            formatter.format(Instant.now()), UUID.randomUUID());
+        loader.load(request, label);
         batch.clear();
     }
 
